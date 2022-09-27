954603e3651eb7b663de3dc2154a6ffeb6392f75
Haim Yeshurun <haimyess@gmail.com>
2022-09-27 21:45:33 +0300

    Added categories section homepage

--- client/src/layout/SharedLayout.js
+++ client/src/layout/SharedLayout.js
@@ -5,11 +5,15 @@ import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
+import "../App.css";
+
 function SharedLayout({ qty }) {
   return (
     <>
       <Header qty={qty} />
-      <Outlet />
+      <div className='app-container'>
+        <Outlet />
+      </div>
       <Footer />
     </>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,44 @@
 /** @format */
 
 import React from "react";
-import Header from "../components/Header";
-// import Banner from "../components/home/Banner";
-// import Grid from "../components/home/Grid";
-import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
+
 import Slider from "../components/Slider";
+
+import "../styles/home.css";
+
+import categories from "../media/categoriesHome";
 
 function Home() {
   return (
-    <>
-      <Slider />
+    <main className='main-wrapper'>
+      <div className='slider-container'>
+        <Slider />
+      </div>
+      <h3 className='categories-home-title'>Special Categories</h3>
+      <div className='categories-main'>
+        {categories.slice(0, 5).map((category) => {
+          return (
+            <div className='category-home'>
+              <Link to={category.link}>
+                <div className='image-container'>
+                  <img className='category-img' src={category.image} />
+                </div>
+                <p className='name-categories-home'>{category.title}</p>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+
+      {/* Popular products */}
+
+      <div className='popular-products'>
+        <h3 className='categories-home-title'>Popular Products</h3>
+      </div>
       {/* <Banner />
       <Grid /> */}
-    </>
+    </main>
   );
 }
 
