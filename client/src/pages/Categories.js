b72f313f66bc297033c7827c5d70d432eb0c053a
Haim Yeshurun <haimyess@gmail.com>
2022-10-05 17:50:17 +0300

    added catalogue design and functionality

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -1,40 +1,41 @@
 /** @format */
 import React from "react";
 
-function Categories() {
-  return (
-    <div>
-      <h1>Categories</h1>
+import { Link } from "react-router-dom";
 
-      <div className='grid-wrapper'>
-        <div className='category-grid fruits'>
-          <span className=''>Fruits</span>
-        </div>
-        <div className='category-grid cereals'>
-          <span>Cereals</span>
-        </div>
-        <div className='category-grid grains'>
-          <span>Grains</span>
-        </div>
-        <div className='category-grid alcohol'>Alcohol</div>
-        <div className='category-grid vegan'>
-          <span>Vegan</span>
-        </div>
-        <div className='category-grid drinks'>
-          <span>Drinks</span>
-        </div>
-        <div className='category-grid meat'>
-          <span>Meat</span>
-        </div>
-        <div className='category-grid dairy'>
-          <span>Dairy</span>
-        </div>
-        <div className='category-grid vegetables'>
-          <span>Vegetables</span>
-        </div>
-      </div>
+import "../styles/categories.css";
+
+function Categories() {
+  const gridCategories = [
+    { id: 1, name: "Fruits", class: "fruits" },
+    { id: 2, name: "Cereals", class: "cereals" },
+    { id: 3, name: "Grains", class: "grains" },
+    { id: 4, name: "Alcohol", class: "alcohol" },
+    { id: 5, name: "Vegan", class: "vegan" },
+    { id: 6, name: "Drinks", class: "drinks" },
+    { id: 7, name: "Meat", class: "meat" },
+    { id: 8, name: "Dairy", class: "dairy" },
+    { id: 9, name: "Vegetables", class: "vegetables" },
+  ];
+  return (
+    <div className='grid-wrapper'>
+      {gridCategories.map((category) => {
+        return (
+          <Link
+            to={`/${category.name}`}
+            className={`grid-item  ${category.class}`}>
+            <span className='title-cat'>{category.name}</span>{" "}
+          </Link>
+        );
+      })}
     </div>
   );
 }
 
 export default Categories;
+
+{
+  /* <Link to={`/${category.product_category}`}>
+                <h3>{category.product_category}</h3>
+              </Link> */
+}
